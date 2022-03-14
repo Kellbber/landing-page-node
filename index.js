281b9f06e532cc472c82270df2898197f7170418
Kellbber <kellbberbarcarolo@gmail.com>
2022-03-14 20:54:19 -0300

    primeira metade

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+app.get("/pagina", function(req, res){
+    res.send("pagina 2");
+});
+app.post("/", function(req, res){
+    res.send("Hello, World via post!!!");
+});
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`))
+
